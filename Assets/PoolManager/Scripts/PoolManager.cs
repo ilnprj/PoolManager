353f522891a469b531c723fbbe32ee2ba9a014bc
iln <romanhvorostov87@gmail.com>
2019-07-23 22:54:40 +0700

    git init

---
+++ Assets/PoolManager/Scripts/PoolManager.cs
@@ -0,0 +1,20 @@
+﻿namespace PoolManager
+{
+    using UnityEngine;
+    public class PoolManager : MonoBehaviour
+    {
+        public enum SceneType
+        {
+            OnceLoad, DontDestroy
+        }
+
+        public SceneType TypeLoad;
+
+        private void Awake()
+        {
+
+        }
+    }
+}
+
+
