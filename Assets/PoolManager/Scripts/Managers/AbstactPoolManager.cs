55300ea9cf366c1886db20a1229022787dd1c1fa
iln <romanhvorostov87@gmail.com>
2019-07-27 21:49:28 +0700

    Backup

---
+++ Assets/PoolManager/Scripts/Managers/AbstactPoolManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+namespace ManagerPooling
+{
+    public static class AbstactPoolManager<T>
+    {
+        public static Dictionary<string, Pool<T>> AllPools = new Dictionary<string, Pool<T>>();
+
+        public static void CreateNewPool()
+        {
+
+        }
+
+        public static void GetFromPool()
+        {
+
+        }
+
+        public static void BackToPool(string idGroup, T item)
+        {
+            if (AllPools.ContainsKey(idGroup) && AllPools[idGroup].ObjectsInPool.ContainsKey(item))
+            {
+                AllPools[idGroup].ObjectsInPool[item].Free = true;
+            }
+        }
+    }
+}
---
+++ Assets/PoolManager/Scripts/Managers/SimplePooling.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimplePooling : MonoBehaviour
+{
+    public List<GameObject> Pool = new List<GameObject>();
+
+    private void Awake()
+    {
+
+    }
+
+    public void InstallPool(GameObject prefab, int size)
+    {
+
+    }
+
+    public void GetFromPool()
+    {
+
+    }
+
+    public void BackToPool(GameObject obj)
+    {
+
+    }
+}
